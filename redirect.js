bcdc35c368ba757aefe982362c75d5780dcb7c46
Shahworld <147256525+Shahworld@users.noreply.github.com>
2024-01-07 21:04:55 +0530

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,8 +1,8 @@
 // redirect.js
-const currentPage = window.location.pathname;
+const currentPage = window.location.index.html;
 
 // Check if the current page is not the home page
-if (currentPage !== '/') {
+if (currentPage !== '/index.html') {
     // Redirect to the maintenance page
     window.location.replace('/maintenance.html');
 }
