d569489095f12e7831fc2d38107e3557822fc04c
Shahworld <147256525+Shahworld@users.noreply.github.com>
2024-01-08 15:48:41 +0530

    Create imdex.jsx

---
+++ imdex.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website!</h1>
+      <p>Line 1: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Line 2: Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <p>Line 3: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      // ...
+      // Add more lines as needed
+      // ...
+      <p>Line 90: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+  );
+};
+
+export default HomePage;
